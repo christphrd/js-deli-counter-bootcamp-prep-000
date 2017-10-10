83ea348f1bc3917acdce6cefb297bf3794a095ec
Christopher Diep <diep.christopher@gmail.com>
2017-10-10 23:25:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(line){
 }
 
 function currentLine(line){
-  if (line == []){
+  if (line.length == 0){
     return "The line is currently empty."
   } else {
     return "not right" // place holder for now. Is this a loop?
