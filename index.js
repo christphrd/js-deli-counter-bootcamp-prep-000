83f244df558650b1f8ee69fea2519b2822e2e052
Christopher Diep <diep.christopher@gmail.com>
2017-10-10 23:30:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function currentLine(line){
   if (line.length == 0){
     return "The line is currently empty."
   } else {
-    return "The line is currently:" + do {
+    return "The line is currently:" + do
+    {
       var i = 1
       i+". "+line[i]
       i++
